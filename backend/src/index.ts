5c40c82065187e70b3b63465f5b6b6a6c63c4f3c
Maximilian Seifert <maximilian.cypher@gmail.com>
2023-08-02 10:57:48 +0200

    Add basic SQLite database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
+import sqlite3 from 'sqlite3';
 
 const app = express();
 const port = 9001;
 
 app.get('/', (_req, res) => {
-  res.send('Hello World');
+  const database = new sqlite3.Database('database.db', sqlite3.OPEN_READONLY);
+
+  database.get('SELECT 1 FROM Subcontractors', (error, row) => {
+    if (error) {
+      throw error;
+    }
+
+    res.send(row);
+  });
 });
 
 app.listen(port, () => {
